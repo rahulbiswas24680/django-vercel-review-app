4add8c1cc111a08370d4c898bfa182bd2f5d6d38
rahulbiswas <rahulbiswas1682001@gmail.com>
2023-06-22 22:22:36 +0530

    updated urls

--- project/urls.py
+++ project/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('review/', include('review.urls'))
+    path('review/', include('review.urls')),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
 
 if settings.DEBUG:
